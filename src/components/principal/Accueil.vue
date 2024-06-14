cb521c1ee3fd6697471c5520c3333702081d0a5c
tafita37 <taf.rand37@gmail.com>
2024-06-14 12:37:56 +0300

    Integration template et vue partie 2

--- src/components/principal/Accueil.vue
+++ src/components/principal/Accueil.vue
@@ -2,7 +2,7 @@
 </script>
 <template>
     <div>
-        
+        fdffd
     </div>
 </template>
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Accueil from '@/components/principal/Accueil.vue'
+import Accueil from '../components/principal/Accueil.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'accueil',
       component: Accueil
     },
     {
